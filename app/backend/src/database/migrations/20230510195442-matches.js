f01d4c8d33ebaf35fca988578f74acefd62ecf07
Frederico Tavares <fredericotp@hotmail.com>
2023-05-10 17:34:43 -0300

    Feat: Requisito 13

---
+++ app/backend/src/database/migrations/20230510195442-matches.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('matches', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+      },
+      home_team_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'teams',
+          key: 'id',
+        }
+      },
+      home_team_goals: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      away_team_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'teams',
+          key: 'id',
+        }
+      },
+      away_team_goals: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      in_progress: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+      },
+    })
+  },
+
+  async down (queryInterface, _Sequelize) {
+    await queryInterface.dropTable('matches');
+  }
+};
---
+++ app/backend/src/database/models/MatchModel.ts
@@ -0,0 +1,52 @@
+import { Model, INTEGER, BOOLEAN } from "sequelize";
+import db from '.';
+
+import TeamModel from "./TeamModel";
+
+class MatchModel extends Model {
+  declare id: number;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+MatchModel.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeamId: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamId: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  }
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+  underscored: true,
+});
+
+MatchModel.belongsTo(TeamModel, { foreignKey: "homeTeamId", as: "homeTeam" });
+MatchModel.belongsTo(TeamModel, { foreignKey: "awayTeamId", as: "awayTeam" });
+
+export default MatchModel;
